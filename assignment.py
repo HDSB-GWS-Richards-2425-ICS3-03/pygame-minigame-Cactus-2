a139c00accb2ad01d46a4d921028508862aa5564
Cactus-2 <1romerolonic@hdsb.ca>
2025-04-08 11:08:11 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -24,9 +24,9 @@ clock = pygame.time.Clock()
 
 target_surface = pygame.Surface((100,100))
 target_surface.fill('Red')
-target_x = 100
-target_y = 100
-target_surface_area = ((target_x,target_y),(target_x + 100,target_y + 100))
+target_pos = [100, 100]
+target_end = [target_pos[0] + 100, target_pos[1] + 100]
+target_surface_area = [target_pos,target_end]
 
 
 while True:
@@ -34,15 +34,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    if event.type == pygame.MOUSEBUTTONDOWN and event.type != pygame.NOEVENT:
+    if event.type == pygame.MOUSEBUTTONDOWN:
         mouse_pos = event.dict["pos"]
-        if (mouse_pos)in target_surface_area:
+        if (mouse_pos) in target_surface_area:
             print ("a")
         else:
             continue
 
    
-    screen.blit(target_surface,(target_x, target_y))
+    screen.blit(target_surface,(target_pos))
 
 
     pygame.display.update()
